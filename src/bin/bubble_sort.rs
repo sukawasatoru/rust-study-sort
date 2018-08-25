ba24ba0ef6dc46d587afba53b753033e78253af9
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2018-08-25 18:55:39 +0900

    Fix bubble sort

--- src/bin/bubble_sort.rs
+++ src/bin/bubble_sort.rs
@@ -4,6 +4,8 @@ extern crate log;
 extern crate rust_study_sort;
 
 use rust_study_sort::testdata::testdata::get_list1;
+use rust_study_sort::testdata::testdata::get_list2;
+use rust_study_sort::testdata::testdata::get_list3;
 
 fn main() {
     env_logger::init();
@@ -20,9 +22,9 @@ fn main() {
 
 fn bubble_sort(src: &mut [i32]) {
     let right = src.len() - 1;
-    for i in 0..right {
+    for i in 0..=right {
         debug!("{:?}", src);
-        for j in 1..(right - i) {
+        for j in 1..=(right - i) {
             if src[j] < src[j - 1] {
                 debug!("swap {}, {}", src[j - 1], src[j]);
 
@@ -37,4 +39,12 @@ fn test_bubble_sort() {
     let mut data = get_list1();
     bubble_sort(&mut data.data);
     assert!(data.data == data.expect, format!("{:?}", data.data));
+
+    let mut data2 = get_list2();
+    bubble_sort(&mut data2.data);
+    assert!(data2.data == data2.expect, format!("{:?}", data2.data));
+
+    let mut data3 = get_list3();
+    bubble_sort(&mut data3.data);
+    assert!(data3.data == data3.expect, format!("{:?}", data3.data));
 }
--- src/testdata/testdata.rs
+++ src/testdata/testdata.rs
@@ -8,6 +8,21 @@ pub fn get_list1() -> TestData {
     }
 }
 
+pub fn get_list2() -> TestData {
+    TestData {
+        data: vec![2, 2, 2, 1, 2, 3],
+        expect: vec![1, 2, 2, 2, 2, 3],
+
+    }
+}
+
+pub fn get_list3() -> TestData {
+    TestData {
+        data: vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+        expect: vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    }
+}
+
 pub fn get_list1u() -> TestDataU {
     TestDataU {
         data: vec![1, 5, 4, 7, 2, 10],
