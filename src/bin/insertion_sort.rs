a3e7c9271cb6a6aaccf4e878326f6d420db6ae15
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2018-08-25 15:13:01 +0900

    Add insertion sort

---
+++ src/bin/insertion_sort.rs
@@ -0,0 +1,46 @@
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+extern crate rust_study_sort;
+
+use rust_study_sort::testdata::testdata::get_list1;
+
+fn main() {
+    env_logger::init();
+
+    info!("Hello");
+
+    let mut data = get_list1();
+    info!("pre={:?}", &data.data);
+    insertion_sort(&mut data.data);
+    info!("post={:?}", &data.data);
+
+    info!("Bye");
+}
+
+fn insertion_sort(src: &mut [i32]) {
+    for i in 1..src.len() {
+        if src[i - 1] < src[i] {
+            continue;
+        }
+
+        let mut j = i;
+        let mut tmp = src[j];
+        loop {
+            src[j] = src[j - 1];
+
+            j -= 1;
+            if j <= 0 || src[j - 1] < tmp {
+                break;
+            }
+        }
+        src[j] = tmp;
+    }
+}
+
+#[test]
+fn test_insertion_sort() {
+    let mut data = get_list1();
+    insertion_sort(&mut data.data);
+    assert!(data.data == data.expect, format!("{:?}", data.data));
+}
